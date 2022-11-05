a2abd3d73d10a79f30f44f22af1efe3a27de2620
Nooomer <ampilogov.qaz@gmail.com>
2022-11-05 21:18:40 +0500

    Add user type

--- app/src/main/java/nooomer/tvmedapp/models/AuthModel.kt
+++ app/src/main/java/nooomer/tvmedapp/models/AuthModel.kt
@@ -2,5 +2,6 @@ package nooomer.tvmedapp.models
 /**Authorization response model. Return [token]
  * @param [token] May be "true" or "false" but it not [Boolean]. Return type - [String]*/
 data class AuthModel(
-    var token:String? = null
+    var token:String? = null,
+    var user_type: String? = null
 )
