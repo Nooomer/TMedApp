7b08714bfb51ba005c3dfb6659bb1fac2b239e81
Nooomer <ampilogov.qaz@gmail.com>
2022-11-16 00:13:26 +0500

    Add user id for Users model

--- app/src/main/java/nooomer/tvmedapp/models/AuthModel.kt
+++ app/src/main/java/nooomer/tvmedapp/models/AuthModel.kt
@@ -3,5 +3,6 @@ package nooomer.tvmedapp.models
  * @param [token] May be "true" or "false" but it not [Boolean]. Return type - [String]*/
 data class AuthModel(
     var token:String? = null,
-    var user_type: String? = null
+    var user_type: String? = null,
+    var user_id: String? = null
 )
