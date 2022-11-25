2ccc907300d11b4247c048783807049f347346fa
Nooomer <ampilogov.qaz@gmail.com>
2022-11-25 18:49:11 +0500

    Fix crash while loading TreatmentActivity.kt

--- app/src/main/java/nooomer/tvmedapp/TreatmentActivity.kt
+++ app/src/main/java/nooomer/tvmedapp/TreatmentActivity.kt
@@ -2,21 +2,19 @@ package nooomer.tvmedapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.progressindicator.BaseProgressIndicator
 import com.google.android.material.progressindicator.LinearProgressIndicator
 import kotlinx.coroutines.*
-import nooomer.tvmedapp.RetrifitService.Common
 import nooomer.tvmedapp.RetrifitService.SessionManager
-import nooomer.tvmedapp.models.AuthModel
-import nooomer.tvmedapp.models.PatientModel
+import nooomer.tvmedapp.interfaces.PreferenceDataType
+import nooomer.tvmedapp.interfaces.RetorfitFun
+import nooomer.tvmedapp.models.UserModel
 import nooomer.tvmedapp.models.TreatmentModel
 
-class TreatmentActivity : AppCompatActivity() {
+class TreatmentActivity : AppCompatActivity(), PreferenceDataType, RetorfitFun {
     lateinit var ssm: SessionManager
     lateinit var indicator: LinearProgressIndicator
     lateinit var recyclerView: RecyclerView
@@ -25,14 +23,12 @@ class TreatmentActivity : AppCompatActivity() {
 
     private fun <T> CoroutineScope.asyncIO(ioFun: () -> T) = async(Dispatchers.IO) { ioFun() }
     private var result: List<TreatmentModel?>? = null
-    private var result2: List<PatientModel?>? = null
+    private var result2: List<UserModel?>? = null
     private val scope = CoroutineScope(Dispatchers.Main + Job())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_treatment)
         ssm = SessionManager(this)
-        println(ssm.fetchAuthToken())
-        println(ssm.fetchTokenLifeTime())
         recyclerView = findViewById(R.id.rv_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
         indicator = findViewById(R.id.ProgressIndicator)
@@ -42,32 +38,39 @@ class TreatmentActivity : AppCompatActivity() {
         load()
        val logoutBtn = findViewById<FloatingActionButton>(R.id.out_btn)
         logoutBtn.setOnClickListener{
-            ssm.deleteAll()
+            ssm.clearSession()
         }
     }
 
-    private fun getAllTreatment(): List<TreatmentModel?>? {
-        val mService = Common.retrofitService
-        val call = mService.getAllTreatment("treatment", "Bearer "+ssm.fetchAuthToken())
-        result = call?.execute()?.body()
-        return result
-    }
+
     private fun load() {
-        when (ssm.fetchUserType()) {
+        var result1 = result
+        when (ssm.fetch(USER_TYPE)) {
             "doctor" ->
             {
                 scope.launch {
-                    val def = scope.asyncIO { getAllTreatment() }
-                    //result1 = patientRequest()
-                    def.await()
-                    viewSize = result!!.size
-                    for (i in 0 until viewSize) {
-                        data.add(mutableListOf<String?>(result!![i]?.patientSurename, result!![i]?.doctorSurname, result!![i]?.startdate))
-                        recyclerView.adapter = RvAdapter(data, viewSize)
-                        recyclerView.adapter?.notifyDataSetChanged()
+                        val def = scope.asyncIO {
+                            result =
+                                get(
+                                    "all",
+                                    "treatment","Bearer "+ssm.fetch(USER_TOKEN)
+                                ) as List<TreatmentModel?>?
+                        }
+                            def.await()
+                        viewSize = result!!.size
+                        for (i in 0 until viewSize) {
+                            data.add(
+                                mutableListOf<String?>(
+                                    result!![i]?.patientSurename,
+                                    result!![i]?.doctorSurname,
+                                    result!![i]?.startdate
+                                )
+                            )
+                            recyclerView.adapter = RvAdapter(data, viewSize)
+                            recyclerView.adapter?.notifyDataSetChanged()
+                        }
+                        indicator.hide()
                     }
-                    indicator.hide()
-                }
             }
             "patient"->{
 
