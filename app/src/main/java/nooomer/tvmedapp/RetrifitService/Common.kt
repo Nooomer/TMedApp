3256a4414996156d7c8fe51a31292f45062bebee
Nooomer <ampilogov.qaz@gmail.com>
2022-11-25 18:46:25 +0500

    Update host link

--- app/src/main/java/nooomer/tvmedapp/RetrifitService/Common.kt
+++ app/src/main/java/nooomer/tvmedapp/RetrifitService/Common.kt
@@ -4,7 +4,7 @@ import com.tvmedicine.RetrifitService.RetrofitClient
 import nooomer.tvmedapp.API
 
 object Common {
-    private const val BASE_URL = "https://ecd7-85-26-165-155.eu.ngrok.io"
+    private const val BASE_URL = "https://tvmed.herokuapp.com"
     val retrofitService: API
         get() = RetrofitClient.getClient(BASE_URL).create(API::class.java)
 }
